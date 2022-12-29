686a301f0e1982fd084062ba3fff71c85f9f7e0a
Ian Brown <ian.brown@spri.ng>
2022-12-29 16:31:02 -0700

    feat: added results page

--- components/date.tsx
+++ components/date.tsx
@@ -1,4 +1,11 @@
-// export default function Date({ dateString: string }) {
-//   const date = parseISO(dateString);
-//   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
-// }
+import { format, parseISO } from 'date-fns';
+
+const Date: React.FC<{ dateString: string }> = ({
+  dateString,
+  ...divProps
+}) => {
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+};
+
+export default Date;
