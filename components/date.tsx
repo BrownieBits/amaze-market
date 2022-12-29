59818c41124be3ec33b80ba6261dce0f492648a4
Ian Brown <ian.brown@spri.ng>
2022-12-29 16:20:41 -0700

    feat: added results page

---
+++ components/date.tsx
@@ -0,0 +1,4 @@
+// export default function Date({ dateString: string }) {
+//   const date = parseISO(dateString);
+//   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+// }
---
+++ components/layouts/base/layout.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import styles from './layout.module.css';
+
+export interface IPrimaryLayout extends React.ComponentPropsWithoutRef<'div'> {}
+export const siteTitle = 'Amaze Market';
+const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }) => {
+  return (
+    <div className={styles.mainWrapper}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="The Amaze Marketplace" />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      {/* <Header /> */}
+      <div className={styles.mainBox}>
+        {/* <Menu /> */}
+        <main className={styles.mainContent}>{children}</main>
+      </div>
+    </div>
+  );
+};
+
+export default PrimaryLayout;
